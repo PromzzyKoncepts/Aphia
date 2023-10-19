d260124e3fd3386481de669deb5d656df0176037
Promise Okechukwu <okechukwupromise638@gmail.com>
2023-10-19 17:09:57 +0100

    Moduify file with styles for cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,14 +6,15 @@ const Cart = () => {
    const allItems = useSelector(state => state.cart)
    const dispatch = useDispatch()
    return (
-      <div>
-         <h1>Welcome to your cart</h1>
+      <div className="w-10/12 m-auto">
+         <h1 className="text-2xl">Welcome to your cart <span>
+            ({allItems.length}) items</span></h1>
          {allItems.map((item, id) => {
             return (
                <div className="cart-content" key={id}>
-                  <img src={item.image} alt={item.title} />
+                  <img src={item.image} alt={item.title} className="w-10" />
                   <h3>{item.title}</h3>
-                  <button onClick={()=> dispatch(remove(item.id))}>Remove</button>
+                  <button className="bg bg-orange-500 text p-2 rounded text-slate-100" onClick={()=> dispatch(remove(item.id))}>Remove</button>
                </div>
             )
          })}
