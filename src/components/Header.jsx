b7ebecf8e82e9de907e8a4626526b80f8ff6e8cf
Promise Okechukwu <okechukwupromise638@gmail.com>
2023-10-29 15:00:51 +0100

    Modify file for responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
-import React, { useContext, useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import aphia from "../assets/aphia.png";
-import jumiagif from "../assets/Jumiagif.gif";
 import { useSelector } from "react-redux";
 import userContext from "../context/userContext";
 
@@ -11,6 +10,7 @@ import IconButton from "@mui/material/IconButton";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import SearchIcon from "@mui/icons-material/Search";
+import { Burger, Menu, Overlay } from "./hamburger";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -21,104 +21,137 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
   },
 }));
 
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
   const { authUser } = useContext(userContext);
   const [isCategories, setIsCategories] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
   const handleCategories = () => setIsCategories((prev) => !prev);
-  const allCartItems = useSelector((state) => state.cart.items); //access the cart proprty of the reducer in the store
-  return (
-    <div>
-      <header>
-        <img src={aphia} alt="aphia logo" className="header_logo"></img>
+  const allCartItems = useSelector((state) => state.cart.items);
 
-        <div class="wrap">
-          <div class="search">
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`bg-slate-50 ${
+        isScrolled ? "fixed w-full top-0  z-[10000000]" : "relative "
+      } py-[0.6rem]`}
+    >
+      <header className=" text-gray-800 flex  justify-between md:justify-evenly  items-center  m-auto">
+        <Link to="/">
+          <img
+            src={aphia}
+            alt="aphia logo"
+            className=" max-w-[9rem] pl-7 md:pl-0 object-contain"
+          ></img>
+        </Link>
+
+        <div className="wrap desktop">
+          <div className="search">
             <input
               type="text"
-              class="searchTerm"
+              className="searchTerm"
               placeholder="Search for products, categories, brands"
             />
 
-            <button type="submit" class="searchButton">
+            <button type="submit" className="searchButton">
               <SearchIcon />
             </button>
           </div>
         </div>
 
-        <nav className="flex flex-row justify-center items-center">
+        <nav className="flex flex-row justify-between gap-4 items-center desktop">
           <div>
-            <NavLink className="text-black" to="/">
-              Home
-            </NavLink>
-          </div>
-          <div>
-          <p
-            className="items-center"
-            onMouseEnter={handleCategories}
-            onMouseLeave={handleCategories}
-          >
-            {authUser ? ` Hi ${authUser.username}` : "My account"}
-            <ArrowDropDownIcon />
-            {isCategories && (
-              <section className="categories rounded">
-                <p>
-                  {authUser ? (
-                    <div className="flex flex-col">
-                      <NavLink className="rounded text-white" to="/orders">
-                        Orders
-                      </NavLink>{" "}
-                      <br />
-                      <NavLink
-                        className="bg-amber-500 p-2 rounded text-white"
-                        to="/logout"
-                      >
-                        Log Out
-                      </NavLink>
-                    </div>
-                  ) : (
-                    <>
-                      <p className=" mb-3">
+            <div
+              className="items-center relative font-semibold cursor-pointer"
+              onMouseEnter={handleCategories}
+              onMouseLeave={handleCategories}
+            >
+              {authUser ? ` ${authUser.username}` : "My account"}
+              <ArrowDropDownIcon />
+              {isCategories && (
+                <section className=" z-[10000] absolute bg-[#333237aa] px-3 py-3 items-center rounded">
+                  <div>
+                    {authUser ? (
+                      <div className="flex flex-col items-center">
                         <NavLink
-                          className="bg-amber-500 p-2 rounded text-white"
-                          to="/login"
+                          className="rounded no-underline text-white"
+                          to="/orders"
                         >
-                          Login
+                          Orders
                         </NavLink>
-                      </p>
-                      <p>
-                        <NavLink className="text-white" to="/register">
-                          SignUp
+                        <br />
+                        <NavLink
+                          className="bg-amber-500 no-underline p-2 hover:bg-[#FF8D3A] rounded text-white"
+                          to="/logout"
+                        >
+                          Log Out
                         </NavLink>
-                      </p>
-                    </>
-                  )}
-                </p>
-
-                
-              </section>
-            )}
-          </p>
+                      </div>
+                    ) : (
+                      <>
+                        <div className="mb-3">
+                          <NavLink
+                            className="bg-amber-500 hover:bg-[#FF8D3A] p-2 no-underline rounded text-white"
+                            to="/login"
+                          >
+                            Login
+                          </NavLink>
+                        </div>
+                        <div>
+                          <NavLink
+                            className="text-white no-underline"
+                            to="/register"
+                          >
+                            SignUp
+                          </NavLink>
+                        </div>
+                      </>
+                    )}
+                  </div>
+                </section>
+              )}
             </div>
+          </div>
           <NavLink
             to="/cart"
-            className="hover-icon flex items-center gap-3 text-black"
+            className=" flex items-center gap-2 no-underline text-slate-900"
           >
             <IconButton aria-label="cart">
-              <StyledBadge badgeContent={allCartItems?.length} color="secondary">
+              <StyledBadge badgeContent={allCartItems?.length} color="error">
                 <ShoppingCartIcon />
               </StyledBadge>
-            </IconButton>{" "}
+            </IconButton>
             Cart
           </NavLink>
           <NavLink
-            className="bg-amber-500 px-3 py-1 rounded font-bold text-white"
+            className="bg-amber-500 px-3 py-1 rounded font-bold hover:bg-orange-500 no-underline text-white"
             to="/dashboard"
           >
             Sell
           </NavLink>
         </nav>
+        <div className="hamburger">
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
+          <Overlay open={open} onClick={() => setOpen(false)} />
+        </div>
       </header>
-      <img src={jumiagif} alt="gif" />
     </div>
   );
 };
