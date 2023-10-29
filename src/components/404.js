0d19089fc21ccb1a0de98210dc54bb7bc11d4158
Promise Okechukwu <okechukwupromise638@gmail.com>
2023-10-29 18:07:00 +0100

    responsiveness

--- src/components/404.js
+++ src/components/404.js
@@ -6,9 +6,9 @@ import unfound from "../assets/404.jpg"
 function NotFound() {
   return (
     <div>
-      <section className="grid grid-cols-2 place-items-center mt-9">
+      <section className="md:grid md:grid-cols-2 md:place-items-center md:mt-9">
         <div className="col-span-1 item1">
-          <h1 className=' text-9xl text-orange-500 font-bold pb-6'>Oops!</h1>
+          <h1 className=' md:text-9xl oops text-orange-500 font-bold pb-6'>Oops!</h1>
           <h4>We can't seem to find the page you are looking for.</h4>
           <small>Error code: 404</small>
           <p>Here are some helpful links instead</p>
@@ -23,7 +23,7 @@ function NotFound() {
           
         </div>
         <div className="item2 col-span-1">
-          <img className=' h-full  object-cover' src={unfound} alt='404 not found' />
+          <img className='not_found_image md:h-full  object-cover' src={unfound} alt='404 not found' />
         </div>
       </section>
     </div>
