acbed86371667ea21b0c2530c3560b935398ba06
Promise Okechukwu <okechukwupromise638@gmail.com>
2023-10-29 15:01:08 +0100

    Move gif to home page only

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,17 @@ import { NavLink } from "react-router-dom";
 import Slider from "../components/Slider";
 import Section2 from "../components/Section2";
 import Footer from "../components/Footer";
+import jumiagif from "../assets/Jumiagif.gif";
+
 
 const Home = () => {
   return (
     <div>
+      <img src={jumiagif} className="w-full" alt="gif" />
+
       <Slider />
       <Section2 />
-        
+
       <div></div>
       <Footer />
     </div>
